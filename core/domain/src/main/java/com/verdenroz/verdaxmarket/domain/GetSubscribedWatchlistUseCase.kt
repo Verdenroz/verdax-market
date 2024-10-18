2c88924998a5bfe5e01e547d2c5656d7fe1fe4b3
harvey <harveytseng2@gmail.com>
2024-10-17 21:39:06 -0400

    use flatMapConcat to prevent multiple flows of watchlist

--- core/domain/src/main/java/com/verdenroz/verdaxmarket/domain/GetSubscribedWatchlistUseCase.kt
+++ core/domain/src/main/java/com/verdenroz/verdaxmarket/domain/GetSubscribedWatchlistUseCase.kt
@@ -11,7 +11,15 @@ import com.verdenroz.verdaxmarket.core.data.utils.handleNetworkException
 import com.verdenroz.verdaxmarket.core.model.SimpleQuoteData
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetSubscribedWatchlistUseCase @Inject constructor(
@@ -30,32 +38,36 @@ class GetSubscribedWatchlistUseCase @Inject constructor(
      * Returns a flow of the watchlist data for the given list of symbols
      * If there is an error in the socket, it will manually poll the API
      */
-    operator fun invoke(): Flow<Result<List<SimpleQuoteData>, DataError.Network>> = watchlistRepository.watchlist.flatMapLatest { watchlist ->
-        val symbols = watchlist.map { it.symbol }
-        if (symbols.isEmpty()) {
-            return@flatMapLatest flowOf(Result.Success(emptyList()))
-        }
-        socket.getWatchlist(symbols).flatMapLatest { socketResult ->
-            when (socketResult) {
-                is Result.Success -> {
-                    watchlistRepository.updateWatchList(socketResult.data)
-                    flowOf(Result.Success(socketResult.data))
-                }
-                else -> marketMonitor.isMarketOpen.flatMapLatest { isOpen ->
-                    flow<Result<List<SimpleQuoteData>, DataError.Network>> {
-                        while (true) {
-                            watchlistRepository.updateWatchlist(symbols)
-                            val currentWatchlist = watchlistRepository.getWatchlist().first()
-                            emit(Result.Success(currentWatchlist))
+    operator fun invoke(): Flow<Result<List<SimpleQuoteData>, DataError.Network>> =
+        watchlistRepository.watchlist.flatMapConcat { watchlist ->
+            val symbols = watchlist.map { it.symbol }
+            if (symbols.isEmpty()) {
+                return@flatMapConcat flowOf(Result.Success(emptyList()))
+            }
+            socket.getWatchlist(symbols).flatMapMerge { socketResult ->
+                when (socketResult) {
+                    is Result.Success -> {
+                        watchlistRepository.updateWatchList(socketResult.data)
+                        flowOf(Result.Success(socketResult.data))
+                    }
 
-                            when (isOpen) {
-                                true -> delay(MARKET_DATA_REFRESH_OPEN)
-                                false -> delay(MARKET_DATA_REFRESH_CLOSED)
+                    is Result.Loading -> flowOf(Result.Loading())
+                    else -> marketMonitor.isMarketOpen.flatMapLatest { isOpen ->
+                        flow<Result<List<SimpleQuoteData>, DataError.Network>> {
+                            while (true) {
+                                watchlistRepository.updateWatchlist(symbols)
+                                val currentWatchlist = watchlistRepository.getWatchlist().first()
+                                emit(Result.Success(currentWatchlist))
+
+                                when (isOpen) {
+                                    true -> delay(MARKET_DATA_REFRESH_OPEN)
+                                    false -> delay(MARKET_DATA_REFRESH_CLOSED)
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-    }.flowOn(ioDispatcher).catch { e -> handleNetworkException(e) }
+        }.flowOn(ioDispatcher).catch { e -> handleNetworkException(e) }
+
 }
