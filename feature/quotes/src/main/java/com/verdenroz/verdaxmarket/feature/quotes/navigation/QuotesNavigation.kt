a4a2c4798aafbd080d0bddd3b01fd2119490448c
harvey <harveytseng2@gmail.com>
2024-10-17 21:42:57 -0400

    adds navoption for circular navigation in quotes

--- feature/quotes/src/main/java/com/verdenroz/verdaxmarket/feature/quotes/navigation/QuotesNavigation.kt
+++ feature/quotes/src/main/java/com/verdenroz/verdaxmarket/feature/quotes/navigation/QuotesNavigation.kt
@@ -16,7 +16,11 @@ import com.verdenroz.verdaxmarket.feature.quotes.R
 private const val QUOTE_ARG = "symbol"
 private const val QUOTE_ROUTE = "quote_route"
 
-fun NavController.navigateToQuote(symbol: String, navOptions: NavOptionsBuilder.() -> Unit = {}) {
+fun NavController.navigateToQuote(
+    symbol: String, navOptions: NavOptionsBuilder.() -> Unit = {
+        popUpTo(QUOTE_ROUTE) { inclusive = true }
+    }
+) {
     navigate(createQuoteRoute(symbol)) {
         navOptions()
     }
