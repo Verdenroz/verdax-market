fe67934b6aa1224cf1e06645c0c0ac5927a0c63f
harvey <harveytseng2@gmail.com>
2024-10-23 15:29:38 -0400

    returns DataError itself instead of Result wrapper

--- core/data/src/main/java/com/verdenroz/verdaxmarket/core/data/utils/ExceptionHandler.kt
+++ core/data/src/main/java/com/verdenroz/verdaxmarket/core/data/utils/ExceptionHandler.kt
@@ -5,37 +5,37 @@ import com.verdenroz.verdaxmarket.core.common.error.DataError
 import com.verdenroz.verdaxmarket.core.common.error.DataException
 import com.verdenroz.verdaxmarket.core.common.error.HttpException
 import com.verdenroz.verdaxmarket.core.common.error.NetworkException
-import com.verdenroz.verdaxmarket.core.common.result.Result
 
-fun handleNetworkException(e: Throwable): Result.Error<DataError.Network> {
+fun handleNetworkException(e: Throwable): DataError.Network {
     return when (e) {
         is DataException -> {
             when (e) {
                 is HttpException -> {
                     when (e.code) {
-                        400 -> Result.Error(DataError.Network.BAD_REQUEST)
-                        401, 403 -> Result.Error(DataError.Network.DENIED)
-                        404 -> Result.Error(DataError.Network.NOT_FOUND)
-                        408 -> Result.Error(DataError.Network.TIMEOUT)
-                        429 -> Result.Error(DataError.Network.THROTTLED)
-                        500, 504 -> Result.Error(DataError.Network.SERVER_DOWN)
-                        else -> Result.Error(DataError.Network.UNKNOWN)
+                        400 -> DataError.Network.BAD_REQUEST
+                        401, 403 -> DataError.Network.DENIED
+                        404 -> DataError.Network.NOT_FOUND
+                        408 -> DataError.Network.TIMEOUT
+                        429 -> DataError.Network.THROTTLED
+                        500, 504 -> DataError.Network.SERVER_DOWN
+                        else -> DataError.Network.UNKNOWN
                     }
                 }
-                is NetworkException -> Result.Error(DataError.Network.NO_INTERNET)
-                else -> Result.Error(DataError.Network.UNKNOWN)
+
+                is NetworkException -> DataError.Network.NO_INTERNET
+                else -> DataError.Network.UNKNOWN
             }
         }
-        else -> Result.Error(DataError.Network.UNKNOWN)
+        else -> DataError.Network.UNKNOWN
     }
 }
 
-fun handleLocalException(e: Throwable): Result.Error<DataError.Local> {
+fun handleLocalException(e: Throwable): DataError.Local {
     return when (e) {
         is SQLiteException -> {
-            Result.Error(DataError.Local.DATABASE)
+            DataError.Local.DATABASE
         }
 
-        else -> Result.Error(DataError.Local.UNKNOWN)
+        else -> DataError.Local.UNKNOWN
     }
 }
