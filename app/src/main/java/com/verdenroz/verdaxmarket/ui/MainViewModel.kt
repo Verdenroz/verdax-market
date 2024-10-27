519e87f8becdea177b389a12e507b431417bba02
harvey <harveytseng2@gmail.com>
2024-10-27 15:02:29 -0400

    implements MainViewModel to keep track of uiState

---
+++ app/src/main/java/com/verdenroz/verdaxmarket/ui/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.verdenroz.verdaxmarket.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.verdenroz.verdaxmarket.core.data.repository.UserDataRepository
+import com.verdenroz.verdaxmarket.core.model.UserSetting
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    userDataRepository: UserDataRepository,
+) : ViewModel() {
+    val uiState: StateFlow<MainActivityUiState> = userDataRepository.userSetting.map {
+        MainActivityUiState.Success(it)
+    }.stateIn(
+        scope = viewModelScope,
+        initialValue = MainActivityUiState.Loading,
+        started = SharingStarted.WhileSubscribed(5000L),
+    )
+}
+
+sealed interface MainActivityUiState {
+    data object Loading : MainActivityUiState
+    data class Success(val userSetting: UserSetting) : MainActivityUiState
+}
