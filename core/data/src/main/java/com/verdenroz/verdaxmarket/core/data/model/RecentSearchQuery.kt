90fdd3872bff04cc08edf57ae0e23e2dff4edc34
harvey <harveytseng2@gmail.com>
2024-12-04 08:13:28 -0500

    removes redundancies

--- core/data/src/main/java/com/verdenroz/verdaxmarket/core/data/model/RecentSearchQuery.kt
+++
@@ -1,9 +0,0 @@
-package com.verdenroz.verdaxmarket.core.data.model
-
-import com.verdenroz.verdaxmarket.core.database.model.RecentSearchEntity
-import com.verdenroz.verdaxmarket.core.model.RecentSearchQuery
-
-fun RecentSearchQuery.asEntity() = RecentSearchEntity(
-    query = query,
-    timestamp = timestamp
-)
