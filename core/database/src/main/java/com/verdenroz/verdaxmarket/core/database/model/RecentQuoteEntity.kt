7692f3e309638f00343523d0666290f9de19abbe
harvey <harveytseng2@gmail.com>
2024-12-04 08:14:51 -0500

    refactors to new recentSearch schemas

--- core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/RecentQuoteEntity.kt
+++ core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/RecentQuoteEntity.kt
@@ -3,26 +3,13 @@ package com.verdenroz.verdaxmarket.core.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.verdenroz.verdaxmarket.core.model.RecentQuoteResult
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 @Entity(tableName = "recentQuotes")
 data class RecentQuoteEntity(
     @PrimaryKey val symbol: String,
     val name: String,
-    val price: String,
-    val change: String,
-    val percentChange: String,
-    val logo: String?,
-    @ColumnInfo val timestamp: Instant
-)
-
-fun RecentQuoteEntity.asExternalModel() = RecentQuoteResult(
-    symbol = symbol,
-    name = name,
-    price = price,
-    change = change,
-    percentChange = percentChange,
-    logo = logo,
-    timestamp = timestamp,
+    val logo: String? = null,
+    @ColumnInfo val timestamp: Instant = Clock.System.now()
 )
--- core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/RecentSearchEntity.kt
+++ core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/RecentSearchEntity.kt
@@ -3,16 +3,11 @@ package com.verdenroz.verdaxmarket.core.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.verdenroz.verdaxmarket.core.model.RecentSearchQuery
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 @Entity(tableName = "recentSearches")
 data class RecentSearchEntity(
-    @PrimaryKey val query: String,
-    @ColumnInfo val timestamp: Instant
-)
-
-fun RecentSearchEntity.asExternalModel() = RecentSearchQuery(
-    query = query,
-    timestamp = timestamp,
+    @PrimaryKey val searchQuery: String,
+    @ColumnInfo val timestamp: Instant = Clock.System.now(),
 )
