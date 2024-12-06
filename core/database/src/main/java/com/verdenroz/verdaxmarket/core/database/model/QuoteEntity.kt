6d1551a9837c0490e15ef8a067c465ed6e73a86e
harvey <harveytseng2@gmail.com>
2024-12-05 21:59:38 -0500

    refactors QuoteEntity to fit WatchlistQuote

--- core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/QuoteEntity.kt
+++ core/database/src/main/java/com/verdenroz/verdaxmarket/core/database/model/QuoteEntity.kt
@@ -7,10 +7,17 @@ import androidx.room.PrimaryKey
  * Represents individual stock data in a WatchList
  * @param symbol the quote symbol
  * @param order the order in the watchlist
+ * @param name the name of the quote
+ * @param logo the logo of the quote if available
  */
 @Entity(tableName = "quotes")
 data class QuoteEntity(
     @PrimaryKey val symbol: String,
+    val name: String,
+    val price: String?,
+    val change: String?,
+    val percentChange: String?,
+    val logo: String?,
     val order: Int,
 )
 
