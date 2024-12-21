9db9b8a1756148b204785731cc9a978bc9c9384b
harvey <harveytseng2@gmail.com>
2024-12-20 20:41:16 -0500

    prettifies WatchlistQuote and adds more vert for options

--- feature/watchlist/src/main/java/com/verdenroz/verdaxmarket/feature/watchlist/components/WatchlistedQuote.kt
+++ feature/watchlist/src/main/java/com/verdenroz/verdaxmarket/feature/watchlist/components/WatchlistedQuote.kt
@@ -1,7 +1,6 @@
 package com.verdenroz.verdaxmarket.feature.watchlist.components
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -12,13 +11,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,6 +42,7 @@ import com.verdenroz.verdaxmarket.core.designsystem.components.VxmAsyncImage
 import com.verdenroz.verdaxmarket.core.designsystem.components.VxmListItem
 import com.verdenroz.verdaxmarket.core.designsystem.icons.VxmIcons
 import com.verdenroz.verdaxmarket.core.designsystem.theme.ThemePreviews
+import com.verdenroz.verdaxmarket.core.designsystem.theme.VxmTheme
 import com.verdenroz.verdaxmarket.core.designsystem.theme.getNegativeBackgroundColor
 import com.verdenroz.verdaxmarket.core.designsystem.theme.getNegativeTextColor
 import com.verdenroz.verdaxmarket.core.designsystem.theme.getPositiveBackgroundColor
@@ -58,7 +57,9 @@ import kotlin.math.pow
 internal fun WatchlistedQuote(
     quote: WatchlistQuote,
     onClick: () -> Unit,
-    onNavigateToQuote: (String) -> Unit
+    onMoreClick: () -> Unit,
+    onNavigateToQuote: (String) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     // Keep track of tap gesture
     var isTapped by remember { mutableStateOf(false) }
@@ -66,107 +67,124 @@ internal fun WatchlistedQuote(
         delay(300)
         isTapped = false
     }
-    VxmListItem(
-        modifier = Modifier
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                        // Workaround for double tap gesture without the delay
-                        if (isTapped) {
-                            onNavigateToQuote(quote.symbol)
-                        } else {
-                            isTapped = true
-                        }
-                    }
-                )
-            }
-            .clickable { onClick() },
-        leadingContent = {
-            if (quote.logo != null) {
-                VxmAsyncImage(
-                    model = quote.logo!!,
-                    description = stringResource(id = R.string.feature_watchlist_logo_description),
-                    modifier = Modifier
-                        .size(48.dp)
-                        .clip(CircleShape)
-                )
-            } else {
-                Box(
-                    modifier = Modifier
-                        .size(48.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.inverseSurface),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = quote.symbol,
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Black,
-                        letterSpacing = 1.25.sp,
-                        maxLines = 1,
-                        color = MaterialTheme.colorScheme.inverseOnSurface,
-                    )
-                }
-            }
-        },
-        headlineContent = {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        IconButton(
+            onClick = onMoreClick,
+            colors = IconButtonDefaults.iconButtonColors(
+                contentColor = MaterialTheme.colorScheme.onSurface
+            ),
+        ) {
+            Icon(
+                imageVector = VxmIcons.More,
+                contentDescription = stringResource(id = R.string.feature_watchlist_more),
+            )
+        }
+
+        if (quote.logo != null) {
+            VxmAsyncImage(
+                model = quote.logo!!,
+                description = stringResource(id = R.string.feature_watchlist_logo_description),
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+            )
+        } else {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.inverseSurface),
+                contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = quote.name,
+                    text = quote.symbol,
                     style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    textAlign = TextAlign.Start,
-                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Black,
+                    letterSpacing = 1.25.sp,
                     maxLines = 1,
-                    modifier = Modifier
-                        .weight(.5f)
-                        .padding(end = 8.dp)
+                    color = MaterialTheme.colorScheme.inverseOnSurface,
                 )
-                if (quote.price != null && quote.change != null && quote.percentChange != null) {
-                    Column(
-                        modifier = Modifier.weight(.3f),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = quote.price!!,
-                            style = MaterialTheme.typography.titleMedium,
-                            fontWeight = FontWeight.SemiBold,
-                            color = MaterialTheme.colorScheme.onSurfaceVariant,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            textAlign = TextAlign.End
-                        )
-                        Text(
-                            text = quote.change!!,
-                            style = MaterialTheme.typography.titleSmall,
-                            color = if (quote.change?.startsWith("-") == true) getNegativeTextColor() else getPositiveTextColor(),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            textAlign = TextAlign.End
-                        )
-                    }
-                    Text(
-                        text = quote.percentChange!!,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = if (quote.percentChange?.startsWith("-") == true) getNegativeTextColor() else getPositiveTextColor(),
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .clip(RoundedCornerShape(50))
-                            .background(if (quote.percentChange?.startsWith("-") == true) getNegativeBackgroundColor() else getPositiveBackgroundColor())
-                            .padding(4.dp)
-                            .weight(.25f)
+            }
+        }
+        VxmListItem(
+            modifier = modifier
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = {
+                            // Workaround for double tap gesture without the delay
+                            if (isTapped) {
+                                onNavigateToQuote(quote.symbol)
+                            } else {
+                                isTapped = true
+                            }
+                        }
                     )
                 }
-            }
-        },
-
+                .clickable { onClick() },
+            headlineContent = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = quote.name,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        textAlign = TextAlign.Start,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        modifier = Modifier
+                            .weight(.5f)
+                            .padding(end = 8.dp)
+                    )
+                    if (quote.price != null && quote.change != null && quote.percentChange != null) {
+                        Column(
+                            modifier = Modifier.weight(.3f),
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = quote.price!!,
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.SemiBold,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                textAlign = TextAlign.End
+                            )
+                            Text(
+                                text = quote.change!!,
+                                style = MaterialTheme.typography.titleSmall,
+                                color = if (quote.change?.startsWith("-") == true) getNegativeTextColor() else getPositiveTextColor(),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                textAlign = TextAlign.End
+                            )
+                        }
+                        Text(
+                            text = quote.percentChange!!,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = if (quote.percentChange?.startsWith("-") == true) getNegativeTextColor() else getPositiveTextColor(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(50))
+                                .background(if (quote.percentChange?.startsWith("-") == true) getNegativeBackgroundColor() else getPositiveBackgroundColor())
+                                .padding(4.dp)
+                                .weight(.25f)
+                        )
+                    }
+                }
+            },
         )
+    }
 }
 
 @Composable
@@ -177,7 +195,6 @@ internal fun EditableWatchlistQuote(
     modifier: Modifier = Modifier
 ) {
     var offsetX by remember { mutableFloatStateOf(0f) }
-    var offsetY by remember { mutableFloatStateOf(0f) }
     var isDragging by remember { mutableStateOf(false) }
     val maxDragDistance = 500f
     val dragProportion = abs(offsetX) / maxDragDistance
@@ -185,15 +202,12 @@ internal fun EditableWatchlistQuote(
         targetValue = (dragProportion.pow(2)).coerceIn(0.00001f, 1f),
         label = "weight"
     )
-    val elevation by animateDpAsState(if (isDragging) 8.dp else 0.dp, label = "elevation")
     val backgroundColor by animateColorAsState(
-        targetValue = if (isDragging) MaterialTheme.colorScheme.primary.copy(alpha = 0.1f) else MaterialTheme.colorScheme.surfaceVariant,
+        targetValue = if (isDragging) MaterialTheme.colorScheme.surface.copy(alpha = 0.1f) else MaterialTheme.colorScheme.surface,
         label = "backgroundColor"
     )
     Row(
         modifier = modifier
-            .offset(y = offsetY.dp)
-            .shadow(elevation, RoundedCornerShape(25))
             .background(backgroundColor)
             .pointerInput(Unit) {
                 detectHorizontalDragGestures(
@@ -232,21 +246,27 @@ internal fun EditableWatchlistQuote(
             modifier = Modifier
                 .weight((1f - weight).coerceAtLeast(0.01f))
                 .padding(8.dp)
-                .clip(RoundedCornerShape(25))
-                .background(MaterialTheme.colorScheme.surfaceVariant),
+                .clip(RoundedCornerShape(25)),
             contentAlignment = Alignment.Center
         ) {
             WatchlistedQuote(
                 quote = quote,
                 onClick = { },
+                onMoreClick = { },
                 onNavigateToQuote = { }
             )
         }
         IconButton(
             modifier = dragModifier,
-            onClick = {},
+            colors = IconButtonDefaults.iconButtonColors(
+                contentColor = MaterialTheme.colorScheme.onSurface
+            ),
+            onClick = { },
         ) {
-            Icon(VxmIcons.DragHandle, contentDescription = "Reorder")
+            Icon(
+                imageVector = VxmIcons.DragHandle,
+                contentDescription = "Reorder"
+            )
         }
     }
 }
@@ -254,17 +274,40 @@ internal fun EditableWatchlistQuote(
 @ThemePreviews
 @Composable
 private fun PreviewWatchlistedQuote() {
-    EditableWatchlistQuote(
-        quote = WatchlistQuote(
-            symbol = "AAPL",
-            name = "Apple Inc.",
-            price = "$145.86",
-            change = "+0.86",
-            percentChange = "+0.59%",
-            logo = null,
-            order = 0
-        ),
-        dragModifier = Modifier,
-        onDelete = { },
-    )
+    VxmTheme {
+        WatchlistedQuote(
+            quote = WatchlistQuote(
+                symbol = "AAPL",
+                name = "Apple Inc.",
+                price = "$145.86",
+                change = "+0.86",
+                percentChange = "+0.59%",
+                logo = null,
+                order = 0
+            ),
+            onClick = { },
+            onMoreClick = { },
+            onNavigateToQuote = { }
+        )
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun PreviewEditableWatchlistedQuote() {
+    VxmTheme {
+        EditableWatchlistQuote(
+            quote = WatchlistQuote(
+                symbol = "AAPL",
+                name = "Apple Inc.",
+                price = null,
+                change = null,
+                percentChange = null,
+                logo = null,
+                order = 0
+            ),
+            dragModifier = Modifier,
+            onDelete = { },
+        )
+    }
 }
