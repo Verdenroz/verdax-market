f89dc085ba6d31f93180e74c23305cd34e33236b
harvey <harveytseng2@gmail.com>
2024-12-23 23:19:38 -0500

    deletes redundant HoursSocket

--- core/network/src/main/java/com/verdenroz/verdaxmarket/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/verdenroz/verdaxmarket/core/network/di/NetworkModule.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.verdenroz.verdaxmarket.core.network.BuildConfig
 import com.verdenroz.verdaxmarket.core.network.FinanceQueryDataSource
 import com.verdenroz.verdaxmarket.core.network.client.ImplFinanceQueryDataSource
-import com.verdenroz.verdaxmarket.core.network.sockets.HoursSocket
 import com.verdenroz.verdaxmarket.core.network.sockets.MarketSocket
 import com.verdenroz.verdaxmarket.core.network.sockets.ProfileSocket
 import com.verdenroz.verdaxmarket.core.network.sockets.QuoteSocket
@@ -95,15 +94,6 @@ object NetworkModule {
         json: Json,
     ): QuoteSocket {
         return QuoteSocket(json, client)
-    }
-
-    @Provides
-    @Singleton
-    fun provideHoursSocket(
-        client: OkHttpClient,
-        json: Json,
-    ): HoursSocket {
-        return HoursSocket(json, client)
     }
 
     /**
--- core/network/src/main/java/com/verdenroz/verdaxmarket/core/network/model/HoursInfoResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.verdenroz.verdaxmarket.core.network.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class HoursInfoResponse(
-    val status: String,
-    val reason: String,
-)
