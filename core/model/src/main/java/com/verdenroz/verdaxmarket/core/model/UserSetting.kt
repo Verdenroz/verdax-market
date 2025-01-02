801bacfbc07a3505fcf4f7a1ea54a247ab346bd5
harvey <harveytseng2@gmail.com>
2025-01-01 22:40:29 -0500

    replaces notificationsEnabled, analytics, and onboarding settings with regionPreference

--- core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/UserSetting.kt
+++ core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/UserSetting.kt
@@ -2,10 +2,8 @@ package com.verdenroz.verdaxmarket.core.model
 
 data class UserSetting(
     val themePreference: ThemePreference = ThemePreference.SYSTEM,
-    val notificationsEnabled: Boolean = true,
+    val regionPreference: RegionFilter = RegionFilter.US,
     val hintsEnabled: Boolean = true,
     val showMarketHours: Boolean = true,
     val isSynced: Boolean = true,
-    val enableAnonymousAnalytics: Boolean = true,
-    val isOnboardingComplete: Boolean = false
 )
