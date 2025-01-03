06ba6aa279751cb5353ab3c2abac5ae5ac99068b
harvey <harveytseng2@gmail.com>
2025-01-03 10:28:51 -0500

    enables crashlytics mappingFileUpload

--- build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -14,12 +14,9 @@
  *   limitations under the License.
  */
 
-import com.android.build.api.dsl.ApplicationExtension
-import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
 import com.verdenroz.verdaxmarket.logic.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
@@ -37,17 +34,6 @@ class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
                 "implementation"(libs.findLibrary("firebase.analytics").get())
                 "implementation"(libs.findLibrary("firebase.performance").get())
                 "implementation"(libs.findLibrary("firebase.crashlytics").get())
-            }
-
-            extensions.configure<ApplicationExtension> {
-                buildTypes.configureEach {
-                    // Disable the Crashlytics mapping file upload. This feature should only be
-                    // enabled if a Firebase backend is available and configured in
-                    // google-services.json.
-                    configure<CrashlyticsExtension> {
-                        mappingFileUploadEnabled = false
-                    }
-                }
             }
         }
     }
