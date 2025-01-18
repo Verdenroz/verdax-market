70409d35f876d7f7de370ef0236bf20cbbf77afe
harvey <harveytseng2@gmail.com>
2025-01-18 18:51:35 -0500

    creates adaptive AccountDialog

--- feature/settings/src/main/java/com/verdenroz/verdaxmarket/feature/settings/components/AccountDialog.kt
+++ feature/settings/src/main/java/com/verdenroz/verdaxmarket/feature/settings/components/AccountDialog.kt
@@ -6,17 +6,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +26,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,6 +47,7 @@ import com.verdenroz.verdaxmarket.core.designsystem.components.VxmSwitch
 import com.verdenroz.verdaxmarket.core.designsystem.icons.VxmIcons
 import com.verdenroz.verdaxmarket.core.designsystem.theme.ThemePreviews
 import com.verdenroz.verdaxmarket.core.designsystem.theme.VxmTheme
+import com.verdenroz.verdaxmarket.core.designsystem.util.isLandscape
 import com.verdenroz.verdaxmarket.feature.settings.R
 import com.verdenroz.verdaxmarket.feature.settings.UserAuthState
 
@@ -71,10 +71,8 @@ internal fun AccountDialog(
                 .fillMaxWidth()
                 .heightIn(max = 475.dp)
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+            Column {
+                // Header with close button
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -91,118 +89,28 @@ internal fun AccountDialog(
                     }
                 }
 
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .verticalScroll(rememberScrollState())
-                        .padding(horizontal = 16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    if (user.photoUrl.isBlank()) {
-                        Icon(
-                            imageVector = VxmIcons.Account,
-                            contentDescription = stringResource(R.string.feature_settings_profile_picture),
-                            modifier = Modifier
-                                .size(64.dp)
-                                .clip(CircleShape)
-                        )
-                    } else {
-                        VxmAsyncImage(
-                            model = user.photoUrl,
-                            description = stringResource(id = R.string.feature_settings_profile_picture),
-                            modifier = Modifier
-                                .size(64.dp)
-                                .clip(CircleShape),
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    if (user.displayName.isNotBlank()) {
-                        Text(
-                            text = user.displayName,
-                            style = MaterialTheme.typography.titleMedium,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-
-                    Text(
-                        text = user.email,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface
+                // Main content
+                if (isLandscape()) {
+                    AccountDialogLandscapeContent(
+                        user = user,
+                        isSynced = isSynced,
+                        onDeleteAccount = { showDeleteDialog = true },
+                        onSyncChange = onSyncChange
                     )
-
-                    Text(
-                        text = stringResource(id = R.string.feature_settings_created_on) + user.creationDate,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface
+                } else {
+                    AccountDialogPortraitContent(
+                        user = user,
+                        isSynced = isSynced,
+                        onDeleteAccount = { showDeleteDialog = true },
+                        onSyncChange = onSyncChange
                     )
-
-                    Row(
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .clickable { showDeleteDialog = true }
-                            .padding(vertical = 8.dp)
-                    ) {
-                        Icon(
-                            imageVector = VxmIcons.Delete,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.error.copy(alpha = 0.75f),
-                            modifier = Modifier.size(16.dp)
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(
-                            text = stringResource(R.string.feature_settings_delete_account),
-                            style = MaterialTheme.typography.bodySmall,
-                            color = MaterialTheme.colorScheme.error.copy(alpha = 0.75f)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 16.dp),
-                        horizontalArrangement = Arrangement.spacedBy(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = VxmIcons.Sync,
-                            contentDescription = stringResource(R.string.feature_settings_sync),
-                            modifier = Modifier.padding(start = 8.dp)
-                        )
-
-                        Column(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            Text(
-                                text = stringResource(R.string.feature_settings_sync),
-                                style = MaterialTheme.typography.bodyMedium,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-
-                            Text(
-                                text = stringResource(R.string.feature_settings_sync_description),
-                                style = MaterialTheme.typography.bodySmall,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-                        }
-
-                        VxmSwitch(
-                            checked = isSynced,
-                            onCheckedChange = onSyncChange,
-                            modifier = Modifier.padding(end = 8.dp)
-                        )
-                    }
                 }
 
+                // Sign out button
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp),
+                        .padding(8.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Button(
@@ -210,7 +118,9 @@ internal fun AccountDialog(
                         colors = ButtonDefaults.buttonColors(
                             containerColor = MaterialTheme.colorScheme.inverseSurface
                         ),
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
                     ) {
                         Text(
                             text = stringResource(R.string.feature_settings_sign_out),
@@ -222,6 +132,7 @@ internal fun AccountDialog(
         }
     }
 
+    // Dialogs
     if (showSignOutDialog) {
         SignOutDialog(
             onDismiss = { showSignOutDialog = false },
@@ -245,31 +156,292 @@ internal fun AccountDialog(
     if (showEmailReauthDialog) {
         EmailReauthDialog(
             onDismiss = { showEmailReauthDialog = false },
-            onConfirm = {
-                password -> onDeleteAccount(password)
+            onConfirm = { password ->
+                onDeleteAccount(password)
                 showEmailReauthDialog = false
             }
         )
     }
 }
 
-@ThemePreviews
 @Composable
-private fun PreviewAccountDialog() {
-    VxmTheme {
-        AccountDialog(
-            user = UserAuthState.SignedIn(
-                displayName = "John Doe",
-                email = "jojndoe@gmail.com",
-                photoUrl = "",
-                creationDate = "November 10, 2024",
-                providerId = "google.com"
-            ),
-            isSynced = true,
-            onDismiss = {},
-            onSignOut = {},
-            onDeleteAccount = {},
-            onSyncChange = {}
+private fun AccountDialogLandscapeContent(
+    user: UserAuthState.SignedIn,
+    isSynced: Boolean,
+    onDeleteAccount: () -> Unit,
+    onSyncChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .heightIn(max = 200.dp)
+            .wrapContentSize(),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        UserProfileLandscape(
+            user = user,
+            onDeleteAccount = onDeleteAccount,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(vertical = 4.dp, horizontal = 8.dp)
+        )
+
+        VerticalDivider()
+
+        SyncSectionLandscape(
+            isSynced = isSynced,
+            onSyncChange = onSyncChange,
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 4.dp, horizontal = 8.dp)
+        )
+    }
+}
+
+@Composable
+private fun AccountDialogPortraitContent(
+    user: UserAuthState.SignedIn,
+    isSynced: Boolean,
+    onDeleteAccount: () -> Unit,
+    onSyncChange: (Boolean) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        UserProfilePortrait(
+            user = user,
+            onDeleteAccount = onDeleteAccount,
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SyncSectionPortrait(
+            isSynced = isSynced,
+            onSyncChange = onSyncChange,
+        )
+    }
+}
+
+@Composable
+private fun UserProfileLandscape(
+    user: UserAuthState.SignedIn,
+    onDeleteAccount: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            if (user.photoUrl.isBlank()) {
+                Icon(
+                    imageVector = VxmIcons.Account,
+                    contentDescription = stringResource(R.string.feature_settings_profile_picture),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
+                )
+            } else {
+                VxmAsyncImage(
+                    model = user.photoUrl,
+                    description = stringResource(id = R.string.feature_settings_profile_picture),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape),
+                )
+            }
+
+            if (user.displayName.isNotBlank()) {
+                Text(
+                    text = user.displayName,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            if (user.email.isNotBlank()) {
+                Text(
+                    text = user.email,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            Text(
+                text = stringResource(id = R.string.feature_settings_created_on) + user.creationDate,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        DeleteAccountButton(onDeleteAccount = onDeleteAccount)
+    }
+}
+
+@Composable
+private fun UserProfilePortrait(
+    user: UserAuthState.SignedIn,
+    onDeleteAccount: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        if (user.photoUrl.isBlank()) {
+            Icon(
+                imageVector = VxmIcons.Account,
+                contentDescription = stringResource(R.string.feature_settings_profile_picture),
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)
+            )
+        } else {
+            VxmAsyncImage(
+                model = user.photoUrl,
+                description = stringResource(id = R.string.feature_settings_profile_picture),
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape),
+            )
+        }
+
+        if (user.displayName.isNotBlank()) {
+            Text(
+                text = user.displayName,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+        if (user.email.isNotBlank()) {
+            Text(
+                text = user.email,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        Text(
+            text = stringResource(id = R.string.feature_settings_created_on) + user.creationDate,
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        DeleteAccountButton(onDeleteAccount = onDeleteAccount)
+    }
+}
+
+@Composable
+private fun DeleteAccountButton(
+    onDeleteAccount: () -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(onClick = onDeleteAccount)
+            .padding(8.dp)
+    ) {
+        Icon(
+            imageVector = VxmIcons.Delete,
+            contentDescription = stringResource(R.string.feature_settings_delete_account),
+            tint = MaterialTheme.colorScheme.error.copy(alpha = 0.75f),
+            modifier = Modifier.size(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.feature_settings_delete_account),
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.error.copy(alpha = 0.75f)
+        )
+    }
+}
+
+@Composable
+private fun SyncSectionLandscape(
+    isSynced: Boolean,
+    onSyncChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = VxmIcons.Sync,
+            contentDescription = stringResource(R.string.feature_settings_sync),
+            modifier = Modifier.size(48.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.feature_settings_sync),
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        Text(
+            text = stringResource(R.string.feature_settings_sync_description),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        VxmSwitch(
+            checked = isSynced,
+            onCheckedChange = onSyncChange,
+            modifier = Modifier.padding(bottom = 8.dp, top = 16.dp)
+        )
+    }
+}
+
+@Composable
+private fun SyncSectionPortrait(
+    isSynced: Boolean,
+    onSyncChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = VxmIcons.Sync,
+            contentDescription = stringResource(R.string.feature_settings_sync),
+            modifier = Modifier.padding(start = 8.dp)
+        )
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = stringResource(R.string.feature_settings_sync),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Text(
+                text = stringResource(R.string.feature_settings_sync_description),
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        VxmSwitch(
+            checked = isSynced,
+            onCheckedChange = onSyncChange,
+            modifier = Modifier.padding(end = 8.dp)
         )
     }
 }
@@ -369,4 +541,26 @@ private fun EmailReauthDialog(
             }
         }
     )
+}
+
+
+@ThemePreviews
+@Composable
+private fun PreviewAccountDialog() {
+    VxmTheme {
+        AccountDialog(
+            user = UserAuthState.SignedIn(
+                displayName = "John Doe",
+                email = "jojndoe@gmail.com",
+                photoUrl = "",
+                creationDate = "November 10, 2024",
+                providerId = "google.com"
+            ),
+            isSynced = true,
+            onDismiss = {},
+            onSignOut = {},
+            onDeleteAccount = {},
+            onSyncChange = {}
+        )
+    }
 }
