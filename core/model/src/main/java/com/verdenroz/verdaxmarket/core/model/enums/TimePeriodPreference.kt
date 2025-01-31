9004e4adbc34cf5b91a71dd10a72b80af007dc85
harvey <harveytseng2@gmail.com>
2025-01-31 18:19:57 -0500

    creates separate timePeriodPreferences for index and sectors

--- core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/enums/TimePeriodPreference.kt
+++ core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/enums/TimePeriodPreference.kt
@@ -1,13 +1,23 @@
 package com.verdenroz.verdaxmarket.core.model.enums
 
 /**
- * The preferred time periods for market index performance and sector performance
+ * The preferred time periods for market index performance
  */
-enum class TimePeriodPreference {
+enum class IndexTimePeriodPreference {
     ONE_DAY,
     FIVE_DAY,
     ONE_MONTH,
     SIX_MONTH,
+    YEAR_TO_DATE,
+    ONE_YEAR,
+    FIVE_YEAR,
+}
+
+/**
+ * The preferred time periods for sector performance
+ */
+enum class SectorTimePeriodPreference {
+    ONE_DAY,
     YEAR_TO_DATE,
     ONE_YEAR,
     FIVE_YEAR,
