5c025caac34994a842578e5c8ad94df29eb70ca6
harvey <harveytseng2@gmail.com>
2025-01-31 16:47:17 -0500

    adds new TimePeriodPreference for user settings

--- core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/UserSetting.kt
+++ core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/UserSetting.kt
@@ -2,10 +2,13 @@ package com.verdenroz.verdaxmarket.core.model
 
 import com.verdenroz.verdaxmarket.core.model.enums.RegionFilter
 import com.verdenroz.verdaxmarket.core.model.enums.ThemePreference
+import com.verdenroz.verdaxmarket.core.model.enums.TimePeriodPreference
 
 data class UserSetting(
     val themePreference: ThemePreference = ThemePreference.SYSTEM,
     val regionPreference: RegionFilter = RegionFilter.US,
+    val indexTimePeriodPreference: TimePeriodPreference = TimePeriodPreference.ONE_DAY,
+    val sectorTimePeriodPreference: TimePeriodPreference = TimePeriodPreference.SIX_MONTH,
     val hintsEnabled: Boolean = true,
     val showMarketHours: Boolean = true,
     val isSynced: Boolean = true,
---
+++ core/model/src/main/java/com/verdenroz/verdaxmarket/core/model/enums/TimePeriodPreference.kt
@@ -0,0 +1,14 @@
+package com.verdenroz.verdaxmarket.core.model.enums
+
+/**
+ * The preferred time periods for market index performance and sector performance
+ */
+enum class TimePeriodPreference {
+    ONE_DAY,
+    FIVE_DAY,
+    ONE_MONTH,
+    SIX_MONTH,
+    YEAR_TO_DATE,
+    ONE_YEAR,
+    FIVE_YEAR,
+}
